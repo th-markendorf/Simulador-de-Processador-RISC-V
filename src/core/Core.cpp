6c115b5f4f1a16b4eab54d7c18c1d7d534bdfff0
Thiago Markendorf <thima3101@gmail.com>
2025-10-17 16:36:53 -0300

    adicionado exibição do valor decimal nos registradores

--- src/core/Core.cpp
+++ src/core/Core.cpp
@@ -35,7 +35,14 @@ void Core::imprimir_register() {
     std::cout << "Registers:" << std::endl;
     for (int i = 0; i < 32; i += 1) {
         std::cout << std::setw(3) << std::right << "x" << i << ": "
-                << "0x" << std::hex << std::setw(8) << std::setfill('0') << registradores[i] << "   " << std::endl;;
+                  << "0x" << std::hex << std::setw(8) << std::setfill('0') << registradores[i];
+
+        // adiciona o valor em decimal quando nao for zero
+        if (registradores[i] != 0) {
+            std::cout << "  (Decimal: " << std::dec << registradores[i] << ")";
+        }
+
+        std::cout << std::endl;
     }
     std::cout << std::setw(3) << std::right << "prog counter" << ": "
             << "0x" << std::hex << std::setw(8) << std::setfill('0') << contador_programa << std::endl;
